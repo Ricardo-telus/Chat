afe4cd8b60cf44bd4fb8acda7039787252b599d4
Ricardo-telus <ricardo.perez@telus.com>
2022-04-11 09:20:25 -0600

    First commit

--- app.js
+++ app.js
@@ -1,25 +1,95 @@
 const express = require("express");
 const app = express();
 const server = require("http").createServer(app);
-
-const content = require("./content/cards.json");
-
+const { Server } = require("socket.io");
+const io = new Server(server);
+const cors=require("cors")
+var user=[]
+var mess=[]
+var total=0
 // Middlewares
-
+app.use(cors({
+  origin:"*",
+}))
 app.use(express.static("public"));
-
+var bodyParser = require('body-parser');
+const { use } = require("express/lib/application");
+app.use(bodyParser.json()); // for parsing application/json
 // Templating engine setup
-
 app.set("view engine", "ejs");
 
 // Enpoints
-
 app.get("/", (req, res) => {
-  res.render("index", { content });
+  res.render("index");
+});
+  //obtener mensajes
+  app.get("/mes", (req, res) => {
+    try {
+      res.json(mess); 
+    } catch (error) {
+      res.status(400)
+      res.json({response:"something bad happen"})
+    }  
+  });
+    //obtener id
+app.post("/user", (req, res) => {
+  try {
+    res.json({name:findUser(req.body.id)}); 
+  } catch (error) {
+    res.status(400)
+    res.json({response:"something bad happen"})
+  }  
+});
+//cambiar nombre
+app.put("/user", (req, res) => {
+  try {
+    changeName(req.body)
+    res.json({message:"all well"}); 
+  } catch (error) {
+    res.status(400)
+    res.json({response:"something bad happen"})
+  }  
 });
 
-// Starting server.
+//al about socket
+  io.on('connection', (socket) => {    
+    console.log('New user connected, total of current user are:'+(total+=1));
+    user.push([socket.id, user.length+1]) 
+    console.log(user)
+    socket.on('disconnect', () => {
+      console.log('user disconnected, total of current user are:'+(total-=1));
+    });
+  });
+  io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+      console.log('message: ' + msg);
+    });
+  });
+  io.emit('some event', { someProperty: 'some value', otherProperty: 'other value' }); // This will emit the event to all connected sockets
+  io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+      console.log(socket.id)
+      mess.push({user:findUser(socket.id), message:msg})
+      console.log(mess)
+      io.emit('chat message', [findUser(socket.id),msg]);
+    });
+  });
+  // Starting server.
+  server.listen(3000, () => {
+    console.log("Listening on port 3000...");
+  });
 
-server.listen(3000, () => {
-  console.log("Listening on port 3000...");
-});
+  function findUser(id){
+    let name=''
+    user.map((element)=>{
+      element[0]===id&& (name=element[1])
+    })
+    return name
+  } 
+  function changeName(data){
+    console.log(data)
+    user.map((element)=>{
+      String(element[1])===String(data.before)&&(element[1]=data.after)
+    })
+    console.log(user)
+  }
---
+++ public/funciones.js
@@ -0,0 +1,67 @@
+const url='http://localhost:3000/user'
+let soy=''
+var socket = io();
+socket.on('connect',()=>{
+  console.log(`conectado con id: ${socket.id}`)
+  getUser()
+})
+var form = document.getElementById('form');
+var input = document.getElementById('input');
+var padre=document.getElementById("content")
+
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+  if (input.value) {
+    socket.emit('chat message', input.value);
+    input.value = '';
+  }
+});
+
+socket.on('chat message', function(msg) {
+  var nuevo=document.createElement("div");
+  nuevo.classList= String(soy)===String(msg[0])?("col-auto offset-6 alert alert-info rounded-pill text-end"):("col-6 alert alert-success rounded-pill text-start");    
+  nuevo.textContent = String(soy)===String(msg[0])?(msg[1]):(msg[0]+": "+msg[1]);  
+  padre.appendChild(nuevo)
+  window.scrollTo(0, document.body.scrollHeight);
+  console.log(padre)
+}); 
+//Get name
+function getUser(){
+    const Http = new XMLHttpRequest();        
+    Http.open('POST', url);  
+    Http.setRequestHeader("Accept", "application/json");
+    Http.setRequestHeader('Content-Type','application/json');
+    Http.send(JSON.stringify({id:socket.id}));
+    Http.onreadystatechange = function(){
+    if (this.readyState==4 && this.status==200) {
+        data=(JSON.parse(Http.responseText)).name         
+        soy=data
+        document.getElementById('usuario').textContent=" "+data
+    }else if(this.readyState==4 && this.status==400) {
+        cdata=JSON.parse(Http.responseText)
+        console.log(cdata)          
+    }                
+    }
+  }
+  //update name
+  function changeName(){      
+      nuevo=document.getElementById("message-text").value
+      console.log(nuevo)
+     const Http = new XMLHttpRequest();        
+    Http.open('PUT', url);  
+    Http.setRequestHeader("Accept", "application/json");
+    Http.setRequestHeader('Content-Type','application/json');
+    Http.send(JSON.stringify({before:soy, after:nuevo}));
+    Http.onreadystatechange = function(){
+        if (this.readyState==4 && this.status==200) {
+            data=JSON.parse(Http.responseText)      
+            console.log(data)
+            soy=nuevo
+            document.getElementById('usuario').textContent=" "+nuevo
+        }else if(this.readyState==4 && this.status==400) {
+            cdata=JSON.parse(Http.responseText)
+            console.log(cdata)          
+        }                
+    } 
+  }
+
